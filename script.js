eef94e54ba1cb247ccd981e1eb6dc925ea3e7e28
couderc <couderc@wwu.edu>
2023-06-04 18:33:56 -0700

    form working needs formatting

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function jumpToTop(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function FormValidate(){
+    if (document.forms[0].recipeName.value == ""){
+        alert("Name Required");
+        return false;
+    }else if(document.forms[0].category.value == "category"){
+        alert('Category required');
+        return false;
+
+    }else if(document.forms[0].recipe.value == ""){
+        alert('Recipe Required');
+        return false;
+    }
+}
